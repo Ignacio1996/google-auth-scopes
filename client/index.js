4a67653a52bb332f7ad4ca1e980ca231ee6e5349
Nicolas Aguirre <inaguirre96@gmail.com>
2025-01-03 11:20:56 -0500

    client and server working

--- client/index.js
+++ client/index.js
@@ -1,1 +1,11 @@
+const login = async () => {
+  const req = await fetch("http://localhost:8080/authLink", {
+    method: "GET",
+  });
+  const data = await req.json();
+  console.log("redirecting to ", data.authUrl);
 
+  // Redirect to the authLink
+  window.location.href = data.authUrl;
+  document.getElementById("#login").innerHTML = "Logged in!";
+};
